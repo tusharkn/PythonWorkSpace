7392b3911efe8ea9f95e7f43eb5b639f08edb64c
TKN <nath.tushark@gmail.com>
2019-07-29 22:12:14 +0530

    Added Data type example to Beginner driver
    Signed-off-by: TKN <nath.tushark@gmail.com>

---
+++ Beginner/DataType.py
@@ -0,0 +1,12 @@
+VarInt = 10
+VarFloat = 10.5
+VarString = "Tushar"
+
+# Print all the variable and data type
+print (VarInt)
+print (VarFloat)
+print (VarString)
+
+print (type (VarInt))
+print (type (VarFloat))
+print (type (VarString))
